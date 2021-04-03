7dce7de2c1631b5f525e9a7ac5c78acf6b54886a
SahitiManchiraju <77101284+SahitiManchiraju@users.noreply.github.com>
2021-04-03 19:19:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var thunder, thunder1,thunder2,thunder3,thunder4;
+
+var engine, world;
+var drops = [];
+var rand;
+var maxDrops=100;
+
+function preload() {
+    backgroundImg = loadImage("snow3.jpg");
+}
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    createCanvas(1200,700);
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new createDrop(random(0,1200), random(0,1200)));
+        }
+
+    }
+    
+}
+
+function draw(){
+    Engine.update(engine);
+    background(backgroundImg);
+
+        for(var i = 0; i<maxDrops; i++){
+        drops[i].showDrop();
+        drops[i].updateY()
+        
+    }
+
+    drawSprites();
+}   
+
